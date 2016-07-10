20bae834f21bf1edb0b19902c609c7954e2989b2
Dariusz Pawlukiewicz <d.pawlukiewicz93@gmail.com>
2016-07-10 15:53:45 +0200

    fixed untracked files

---
+++ src/AwesomeCalendar.Infrastructure/DependencyInjection/CustomDependencyResolver.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using AwesomeCalendar.Infrastructure.DependencyInjection.Interfaces;
+
+namespace AwesomeCalendar.Infrastructure.DependencyInjection
+{
+    public class CustomDependencyResolver : ICustomDependencyResolver
+    {
+        IContainer Container { get; }
+
+        public CustomDependencyResolver(IContainer container)
+        {
+            Container = container;
+        }
+
+        public TResolved Resolve<TResolved>() => Container.Resolve<TResolved>();
+    }
+}
