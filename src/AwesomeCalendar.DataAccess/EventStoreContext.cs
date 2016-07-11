a4d6914184dca29532c88bfcf2d30753b32bd6fb
Dariusz Pawlukiewicz <d.pawlukiewicz93@gmail.com>
2016-07-11 22:45:13 +0200

    configured TPH in WriteDB

--- src/AwesomeCalendar.DataAccess/EventStoreContext.cs
+++ src/AwesomeCalendar.DataAccess/EventStoreContext.cs
@@ -1,9 +1,16 @@
 ﻿using System.Data.Entity;
+using AwesomeCalendar.Contracts.Events;
 
 namespace AwesomeCalendar.DataAccess
 {
     public class EventStoreContext : DbContext
     {
+        public DbSet<CalendarItemBaseEvent> CalendarItemEvents { get; set; }
+
+        public DbSet<CalendarItemCreatedEvent> CalendarItemCreatedEvents { get; set; }
+
+        public DbSet<CalendarItemCycleCreatedEvent> CalendarItemCycleCreatedEvents { get; set; }
+
         static EventStoreContext()
         {
             Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EventStoreContext>());
@@ -12,6 +19,12 @@ namespace AwesomeCalendar.DataAccess
         public EventStoreContext()
             :base(nameof(EventStoreContext))
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CalendarItemBaseEvent>().HasKey(e => e.Id);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
