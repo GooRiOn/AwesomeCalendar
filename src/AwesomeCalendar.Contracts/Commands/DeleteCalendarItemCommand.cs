ba05eb4cd16d51832aaec2dcdbc3e4d152da49ca
dbarwikowski <d.barwikowski@gmail.com>
2016-07-26 23:19:42 +0200

    Added command for calendar item delete

---
+++ src/AwesomeCalendar.Contracts/Commands/DeleteCalendarItemCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using AwesomeCalendar.Infrastructure.Interfaces.Contracts;
+
+namespace AwesomeCalendar.Contracts.Commands
+{
+    public class DeleteCalendarItemCommand : ICommand
+    {
+        public Guid Id { get; set; }
+    }
+}
