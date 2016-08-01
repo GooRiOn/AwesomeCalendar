033a70c77ac669365e1913ae985992926b07be49
dbarwikowski <d.barwikowski@gmail.com>
2016-08-01 20:43:29 +0200

    Added Delete calendar item test

--- src/AwesomeCalendar.Domain/CommandHandlers/DeleteCalendarItemCommandHandler.cs
+++ src/AwesomeCalendar.Domain/CommandHandlers/DeleteCalendarItemCommandHandler.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AwesomeCalendar.Contracts.Commands;
+using AwesomeCalendar.Infrastructure.Interfaces.DataAccess;
+using AwesomeCalendar.Infrastructure.Interfaces.Handlers;
 
 namespace AwesomeCalendar.Domain.CommandHandlers
 {
-    class DeleteCalendarItemCommandHandler
+    public class DeleteCalendarItemCommandHandler : ICommandHandler<DeleteCalendarItemCommand>
     {
+        private IEventStore EventSotre { get; }
+
+        public DeleteCalendarItemCommandHandler(IEventStore eventStore)
+        {
+            EventSotre = eventStore;
+        }
+
+        public void Handle(DeleteCalendarItemCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Validate(DeleteCalendarItemCommand command)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ tests/AwesomeCalendar.Tests/CommandHandlers/delete_calendar_item_command_handler_tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using AwesomeCalendar.Contracts.Commands;
+using AwesomeCalendar.Contracts.Events;
+using AwesomeCalendar.DataAccess;
+using AwesomeCalendar.Domain.Aggregates;
+using AwesomeCalendar.Domain.CommandHandlers;
+using AwesomeCalendar.Infrastructure.Enums;
+using AwesomeCalendar.Infrastructure.Exceptions;
+using AwesomeCalendar.Infrastructure.Interfaces.DataAccess;
+using AwesomeCalendar.Infrastructure.Interfaces.Handlers;
+using Ploeh.AutoFixture.Xunit2;
+using Shouldly;
+using Xunit;
+
+namespace AwesomeCalendar.Tests.CommandHandlers
+{
+    public class delete_calendar_item_command_handler_tests
+    {
+        private IEventStore EventStore { get; }
+        private ICommandHandler<CreateCalendarItemCommand> CreateCommandHandler { get; }
+        private ICommandHandler<DeleteCalendarItemCommand> DeleteCommandHandler { get; }
+
+        public delete_calendar_item_command_handler_tests()
+        {
+            EventStore = new FakeEventStore();
+            CreateCommandHandler = new CreateCalendarItemCommandHandler(EventStore);
+            DeleteCommandHandler = new DeleteCalendarItemCommandHandler(EventStore);
+        }
+
+        void act(DeleteCalendarItemCommand command)
+        {
+            DeleteCommandHandler.Handle(command);
+        }
+
+        [Theory, AutoData]
+        public void persists_event_to_event_store_needed_to_reconstruct_calendar_item(CreateCalendarItemCommand createCommand, DeleteCalendarItemCommand deleteCommand)
+        {
+            CreateCommandHandler.Handle(createCommand);
+
+            deleteCommand.Id = createCommand.Id;
+
+            act(deleteCommand);
+            var deletedAggreagte = EventStore.GetById<CalendarItem, CalendarItemBaseEvent>(deleteCommand.Id);
+
+            deletedAggreagte.ShouldBeOfType(typeof(CalendarItem));
+            Assert.Equal(deleteCommand.Id, deletedAggreagte.Id);
+        }
+
+        [Fact]
+        public void thors_when_command_is_null()
+        {
+            var exception = Record.Exception(() => act(null));
+
+            exception.ShouldBeOfType(typeof(AwesomeCalendarException));
+            Assert.Equal(((AwesomeCalendarException)exception).Type, AwesomeCalendarExceptionType.NullCommand);
+        }
+
+        [Theory, AutoData]
+        public void throws_when_command_has_empty_id(DeleteCalendarItemCommand command)
+        {
+            command.Id = Guid.Empty;
+
+            var exception = Record.Exception(() => act(command));
+
+            exception.ShouldBeOfType(typeof(AwesomeCalendarException));
+            Assert.Equal(((AwesomeCalendarException)exception).Type, AwesomeCalendarExceptionType.InvalidCommand);
+        }
+    }
+}
