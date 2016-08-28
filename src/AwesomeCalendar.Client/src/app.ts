17d7bc1d4e3409288de81334a7cc1178f5af7ede
Dariusz Pawlukiewicz <d.pawlukiewicz93@gmail.com>
2016-08-28 15:32:24 +0200

    added basic routing

--- src/AwesomeCalendar.Client/src/app.ts
+++ src/AwesomeCalendar.Client/src/app.ts
@@ -1,3 +1,17 @@
-export class App {  
-  message = 'Hello World!';
+import {Router, RouterConfiguration} from 'aurelia-router';
+
+export class App  
+{  
+    router: Router;
+
+    configureRouter(config: RouterConfiguration, router: Router)
+    {
+        config.title = "Awesome Calendar";
+
+        config.map([
+          {route: ['', 'home'], name: 'home', moduleId: 'home', nav: true}
+        ]);
+
+        this.router = router;
+    }  
 }
